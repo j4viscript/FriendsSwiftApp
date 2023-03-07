af30fb768ff1e2fefcc23fdecf9c8cce7a64db25
J4viscript <tezjavi@gmail.com>
2023-03-07 15:19:36 -0700

    Initial Commit

---
+++ FriendsApp/FriendsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsAppApp.swift
+//  FriendsApp
+//
+//  Created by Javier Oskar Murillo Cota on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
